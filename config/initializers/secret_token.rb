b1c6289ad366fa822b71a94d4ade0909d12757f5
James Hicks <jhicks0300@gmail.com>
2011-08-24 21:17:23 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoidMembership::Application.config.secret_token = 'f3f75efc50414aa1baad9ffec8f13820303cbebd90975704de547cc68b3af1191a4e7ffd8c46a3d4ed5456035268054b9ef7305f4ffef3dc94d8f34f77f4e087'
