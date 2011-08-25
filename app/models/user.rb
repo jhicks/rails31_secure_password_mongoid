160bf93ccf1976ff73883b65481ed5eba6d1bf81
James Hicks <jhicks0300@gmail.com>
2011-08-24 22:00:06 -0500

    initial pass at the user model

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User
+  include Mongoid::Document
+  include ActiveModel::SecurePassword
+  has_secure_password
+
+  field :email, :type => String
+  field :password_digest, :type => String
+
+  attr_accessible :email
+  attr_protected :password_digest
+end
