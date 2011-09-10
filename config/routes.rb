8569556707b681918234020b741f8b58f89f616e
James Hicks <jhicks0300@gmail.com>
2011-09-10 13:46:38 -0500

    added registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 MongoidMembership::Application.routes.draw do
   resources :sessions
+  resources :registration
 
   get "login" => "sessions#new", :as => "login"
   post "login" => "sessions#create", :as => "login"
+
+  get "signup" => "registration#new", :as => "signup"
+  post "signup" => "registration#create", :as => "signup"
 
   root :to => 'pages#index'
 end
